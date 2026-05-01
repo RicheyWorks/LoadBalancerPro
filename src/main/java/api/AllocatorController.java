b917e53fdde1b2f4b5f142feea5541ea2aceee1a
richmond423 <richmonddhaenens@student.olympic.edu>
2026-04-30 18:19:25 -0700

    Bump metadata for v1.0-rc3

--- src/main/java/api/AllocatorController.java
+++ src/main/java/api/AllocatorController.java
@@ -14,7 +14,7 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/api")
 public class AllocatorController {
-    private static final String VERSION = "1.0.0-rc2";
+    private static final String VERSION = "1.0.0-rc3";
 
     private final AllocatorService allocatorService;
 

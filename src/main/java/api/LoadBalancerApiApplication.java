30de9afebe43d1b593aa82dae98204a6f12c239e
richmond423 <richmonddhaenens@student.olympic.edu>
2026-05-04 01:51:26 -0700

    Align v2.3.2 release metadata

--- src/main/java/api/LoadBalancerApiApplication.java
+++ src/main/java/api/LoadBalancerApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoadBalancerApiApplication {
-    private static final String FALLBACK_VERSION = "2.3.1";
+    private static final String FALLBACK_VERSION = "2.3.2";
 
     public static void main(String[] args) {
         if (isVersionRequested(args)) {
